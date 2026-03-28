a872ce58d6d5578cf098cbda03a8bbee9f376504
Lucca <lucca.f.livino>
2026-03-27 20:24:25 -0400

    feat: implementando função createTreinador no serviço, incluindo validação e tratamento de erros

--- src/services/treinadorService.ts
+++ src/services/treinadorService.ts
@@ -1,6 +1,9 @@
 import TreinadorRepository from "../repositories/treinadorRepository";
 import { type_treinador } from "../types/dbSchemas";
-import { treinadorIdSchema } from "../utils/validations/treinadorValidation";
+import { treinadorIdSchema, treinadorSchema } from "../utils/validations/treinadorValidation";
+import { ZodError } from "zod";
+import { DatabaseError } from "../utils/errors/DatabaseError";
+import HttpStatusCode from "../utils/helpers/httpStatusCode";
 
 class TreinadorService {
 	private repository: TreinadorRepository;
@@ -21,6 +24,39 @@ class TreinadorService {
 		);
 
 		return treinadores;
+	}
+
+	async createTreinador(novoTreinador: type_treinador): Promise<type_treinador> {
+		try {
+			treinadorSchema.parse(novoTreinador);
+
+			const treinadorExistente = await this.repository.findByUserId(
+				novoTreinador.user_id,
+			);
+
+			if (treinadorExistente) {
+				throw new DatabaseError(
+					"Usuário autenticado já possui perfil de treinador",
+					HttpStatusCode.CONFLICT.code,
+					`Key (user_id)=(${novoTreinador.user_id}) already exists.`,
+					"treinador_user_id_unique",
+					"23505",
+				);
+			}
+
+			const treinadorSanitizado = {
+				...novoTreinador,
+				status_conta: novoTreinador.status_conta ?? true,
+			};
+
+			return await this.repository.create(treinadorSanitizado);
+		} catch (error) {
+			if (error instanceof ZodError) {
+				throw error;
+			}
+
+			throw error;
+		}
 	}
 
 	async getTreinadorById(id: string): Promise<type_treinador> {
